27e745d8d26f1cec6174e790d6f4a12e25136d3d
m1ngshum <140998506+m1ngshum@users.noreply.github.com>
2025-06-20 17:34:07 +0800

    chore(vite.config): Set base path for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import mkcert from "vite-plugin-mkcert";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +9,5 @@ export default defineConfig({
   server: {
     port: 5173,
   },
+  base: process.env.NODE_ENV === 'production' ? '/example/' : '/',
 });
